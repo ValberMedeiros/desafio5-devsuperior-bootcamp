6c90e4a6217bf5440075aa94c021796092a51ada
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-07-10 18:08:28 -0300

    Reviews por movie id!

--- src/main/java/com/devsuperior/movieflix/resources/MovieResource.java
+++ src/main/java/com/devsuperior/movieflix/resources/MovieResource.java
@@ -1,14 +1,18 @@
 package com.devsuperior.movieflix.resources;
 
 import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.dto.ReviewDTO;
 import com.devsuperior.movieflix.services.MovieService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movies")
@@ -30,5 +34,10 @@ public class MovieResource {
     @GetMapping("/{id}")
     public MovieDTO findById(@PathVariable Long id) {
         return service.findById(id);
+    }
+
+    @GetMapping("/{id}/reviews")
+    public ResponseEntity<List<ReviewDTO>> getReviews(@PathVariable Long id) {
+        return ResponseEntity.ok(service.getReviews(id));
     }
 }
--- src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -1,8 +1,11 @@
 package com.devsuperior.movieflix.services;
 
 import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.dto.ReviewDTO;
 import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.entities.Review;
 import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.repositories.ReviewRepository;
 import com.devsuperior.movieflix.services.exception.ResourceNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,15 +13,19 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class MovieService {
 
     private MovieRepository repository;
+    private ReviewRepository reviewRepository;
 
-    public MovieService(MovieRepository repository) {
+    public MovieService(MovieRepository repository, ReviewRepository reviewRepository) {
         this.repository = repository;
+        this.reviewRepository = reviewRepository;
     }
 
     public MovieDTO findById(Long id) {
@@ -40,5 +47,12 @@ public class MovieService {
         }
         return movies
                 .map(MovieDTO::new);
+    }
+
+    public List<ReviewDTO> getReviews(Long movieId) {
+        List<Review> reviews = reviewRepository.findAllByMovieId(movieId);
+        return reviews.stream()
+                .map(ReviewDTO::new)
+                .collect(Collectors.toList());
     }
 }
